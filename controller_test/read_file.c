01efa4ba2c251454e81d5fb54b995487f95e2650
Eric Switzer <eric.switzer@gmail.com>
2012-04-23 18:41:58 -0400

    commit before restructure

--- controller_test/read_file.c
+++ controller_test/read_file.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <hiredis/hiredis.h>
+#include <hiredis/async.h>
+#include <hiredis/adapters/libevent.h>
 #include "uthash.h"
 #define CTRL_KEY_LEN 256
 
@@ -43,12 +48,35 @@ int init_ctrl(void) {
     return 0;
 }
 
-int main ()
-{
+void onMessage(redisAsyncContext *c, void *reply, void *privdata) {
+    redisReply *r = reply;
+    int j;
+    if (reply == NULL) return;
+
+    if (r->type == REDIS_REPLY_ARRAY) {
+        for (j = 0; j < r->elements; j++) {
+            printf("%u) %s\n", j, r->element[j]->str);
+        }
+    }
+}
+
+int main (int argc, char **argv) {
     init_ctrl();
     get_ctrl("this_is_a_name");
     get_ctrl("this_is_b_name");
     get_ctrl("this_is_c_name");
 
+    signal(SIGPIPE, SIG_IGN);
+    struct event_base *base = event_base_new();
+
+    redisAsyncContext *c = redisAsyncConnect("127.0.0.1", 6379);
+    if (c->err) {
+        printf("error: %s\n", c->errstr);
+        return 1;
+    }
+
+    redisLibeventAttach(c, base);
+    redisAsyncCommand(c, onMessage, NULL, "SUBSCRIBE testtopic");
+    event_base_dispatch(base);
     return 0;
 }
--- pyclient/windows.py
+++ pyclient/windows.py
@@ -1,5 +1,4 @@
-# ACT main control interface 
-# Eric Switzer, March 29, 2006. (switzer@princeton.edu)
+r""" Class to manage the creation of system windows"""
 import Tkinter
 import Pmw
 import control_parse
@@ -14,118 +13,143 @@ import input_float_slider
 import input_pulldown
 import input_timerclicker
 GUI_MESSAGE = 'gui_message'
-##
-# \namespace windows
-# Class to manage the creation of system windows 
-#
 
-def compare_cmdorder(x,y):
-  return x['displayorder']-y['displayorder']
+
+def compare_cmdorder(x, y):
+    return x['displayorder'] - y['displayorder']
+
 
 # make a selection - "Go"
-def input_select_name(cmditem, messagequeue, commandqueue, page): 
-  return input_select.InputSelect(cmditem, messagequeue, commandqueue, page)
+def input_select_name(cmditem, messagequeue, commandqueue, page):
+    return input_select.InputSelect(cmditem, messagequeue, commandqueue, page)
+
 
 # Input an on/off signal
-def input_onoff_name(cmditem, messagequeue, commandqueue, page): 
-  return input_onoff.InputOnOff(cmditem, messagequeue, commandqueue, page)
+def input_onoff_name(cmditem, messagequeue, commandqueue, page):
+    return input_onoff.InputOnOff(cmditem, messagequeue, commandqueue, page)
 
-# Integer clicker 
-def input_clicker_name(cmditem, messagequeue, commandqueue, page): 
-  return input_clicker.InputClicker(cmditem, messagequeue, commandqueue, page)
+
+# Integer clicker
+def input_clicker_name(cmditem, messagequeue, commandqueue, page):
+    return input_clicker.InputClicker(cmditem, messagequeue,
+                                      commandqueue, page)
+
 
 # Pulldown selection menu
-def input_pulldown_name(cmditem, messagequeue, commandqueue, page): 
-  return input_pulldown.InputPullDown(cmditem, messagequeue, commandqueue, page)
+def input_pulldown_name(cmditem, messagequeue, commandqueue, page):
+    return input_pulldown.InputPullDown(cmditem, messagequeue,
+                                        commandqueue, page)
+
 
 # Slider to input floats
-def input_float_slider_name(cmditem, messagequeue, commandqueue, page): 
-  return input_float_slider.InputFloatSlider(cmditem, messagequeue, commandqueue, page)
+def input_float_slider_name(cmditem, messagequeue, commandqueue, page):
+    return input_float_slider.InputFloatSlider(cmditem, messagequeue,
+                                               commandqueue, page)
+
 
 # Input a float
-def input_float_name(cmditem, messagequeue, commandqueue, page): 
-  return input_float.InputFloat(cmditem, messagequeue, commandqueue, page)
+def input_float_name(cmditem, messagequeue, commandqueue, page):
+    return input_float.InputFloat(cmditem, messagequeue,
+                                  commandqueue, page)
+
 
 # Timer clicker method
 def input_timerclicker_name(cmditem, messagequeue, commandqueue, page):
-  return input_timerclicker.InputTimerClicker(cmditem, messagequeue, commandqueue, page)
+    return input_timerclicker.InputTimerClicker(cmditem, messagequeue,
+                                                commandqueue, page)
+
 
 # input strings
-def input_string_name(cmditem, messagequeue, commandqueue, page): 
-  return input_string.InputString(cmditem, messagequeue, commandqueue, page)
+def input_string_name(cmditem, messagequeue, commandqueue, page):
+    return input_string.InputString(cmditem, messagequeue,
+                                    commandqueue, page)
+
 
 # input strings + pulldown options
-def input_string_pulldown_name(cmditem, messagequeue, commandqueue, page): 
-  return input_string_pulldown.InputStringPulldown(cmditem, messagequeue, commandqueue, page)
+def input_string_pulldown_name(cmditem, messagequeue, commandqueue, page):
+    return input_string_pulldown.InputStringPulldown(cmditem, messagequeue,
+                                                     commandqueue, page)
+
 
 type_switch = {
-  "input_select": input_select_name,
-  "input_onoff": input_onoff_name,
-  "input_clicker": input_clicker_name,
-  "input_pulldown": input_pulldown_name,
-  "input_float": input_float_name,
-  "input_float_slider": input_float_slider_name,
-  "input_timerclicker": input_timerclicker_name,
-  "input_string": input_string_name,
-  "input_string_pulldown": input_string_pulldown_name}
+    "input_select": input_select_name,
+    "input_onoff": input_onoff_name,
+    "input_clicker": input_clicker_name,
+    "input_pulldown": input_pulldown_name,
+    "input_float": input_float_name,
+    "input_float_slider": input_float_slider_name,
+    "input_timerclicker": input_timerclicker_name,
+    "input_string": input_string_name,
+    "input_string_pulldown": input_string_pulldown_name}
+
 
 class SystemControlWindow:
-  def __init__(self, parent, db, messagequeue, commandqueue, sysname):
-    self.announce = 'SystemControlWindow: '
-    self.messagequeue = messagequeue
-    self.commandqueue = commandqueue
-    Pmw.Color.changecolor(parent,background = '#3E5D75', foreground = '#FFFFFF')
+    def __init__(self, parent, db, messagequeue, commandqueue, sysname):
+        self.announce = 'SystemControlWindow: '
+        self.messagequeue = messagequeue
+        self.commandqueue = commandqueue
+        Pmw.Color.changecolor(parent, background='#3E5D75',
+                              foreground='#FFFFFF')
 
-    # pack the tabbed menu for each category of control commands
-    self.syswindow = Tkinter.Toplevel()
-    self.syswindow.title(repr(sysname))
-    notebook = Pmw.NoteBook(self.syswindow)
-    notebook.pack(fill = 'both', expand = 1, padx = 10, pady = 10)
+        # pack the tabbed menu for each category of control commands
+        self.syswindow = Tkinter.Toplevel()
+        self.syswindow.title(repr(sysname))
+        notebook = Pmw.NoteBook(self.syswindow)
+        notebook.pack(fill='both', expand=1, padx=10, pady=10)
 
-    # for each category of control for the system, generate a new 
-    # tab, and for each tab, generate all the controls for that category
-    categories = control_parse.find_subgroups(db,'system',sysname,'category')
-    print categories
-    self.command_hierarchy={}
-    for category_item in categories:
-      page = notebook.add(category_item)
-      cmdtag = control_parse.find_commands(db,'system',sysname,'category',category_item)
-      cmdtag.sort(compare_cmdorder)
-      for cmditem in cmdtag:
-        #if cmditem['system'] == sysname and cmditem['category'] == category_item:
-        #print category_item + " " + repr(cmditem)
-        self.command_hierarchy[cmditem['short_name']] = \
-          type_switch.get(cmditem['type'])(cmditem, self.messagequeue, self.commandqueue, page)
-        self.command_hierarchy[cmditem['short_name']].pack(side = Tkinter.TOP, anchor=Tkinter.W)
-        # to pass options not in the command specification (cmditem)
-        # keyword=value_to_pass, 
-    notebook.setnaturalsize()
-    # now collapse the entry methods after the maximum window size is known
-    for category_item in categories:
-      cmdtag = control_parse.find_commands(db, 'system', sysname, 'category', category_item)
-      for cmditem in cmdtag:
-        if cmditem['edit_level'] == 1:
-          self.command_hierarchy[cmditem['short_name']].flattenentry()
-    # put in a refresh order 
-    self.commandqueue.put('refresh')
+        # for each category of control for the system, generate a new
+        # tab, and for each tab, generate all the controls for that category
+        categories = control_parse.find_subgroups(db, 'system',
+                                                  sysname, 'category')
 
-  # refresh method (this changes attributes of a given button to reflect server messages)
-  def refresh_system(self, msg):
-   # take everything following, and including the GUI_MESSAGE tag and 
-   # parse it into button messages 
-   RefreshString = msg[msg.find(GUI_MESSAGE):].split()
-   RefreshShortname = RefreshString[1]
-   RefreshType = RefreshString[2]
-   RefreshVal = RefreshString[3]
-   ButtonsAvailable = self.command_hierarchy.keys()
-   if ButtonsAvailable.count(RefreshShortname) > 1:
-     print 'SystemControlWindow: ambiguous button refresh request, more than one button by this name'
-   else:
-     try: 
-       ButtonsAvailable.index(RefreshShortname)
-       print 'hwid: '+repr(RefreshShortname)+' type: '+repr(RefreshType)+' val: '+repr(RefreshVal)
-       self.command_hierarchy[RefreshShortname].refresh_button(RefreshType, RefreshVal)
-     except ValueError:
-       None
-       #print 'SystemControlWindow: this system has no button named ' +repr(RefreshShortname)
+        print categories
+        self.command_hierarchy = {}
+        for category_item in categories:
+            page = notebook.add(category_item)
+            cmdtag = control_parse.find_commands(db, 'system', sysname,
+                                                 'category', category_item)
 
+            cmdtag.sort(compare_cmdorder)
+            for cmditem in cmdtag:
+                #if cmditem['system'] == sysname and \
+                #   cmditem['category'] == category_item:
+                #print category_item + " " + repr(cmditem)
+                self.command_hierarchy[cmditem['short_name']] = \
+                    type_switch.get(cmditem['type'])(cmditem,
+                                    self.messagequeue, self.commandqueue, page)
+
+                self.command_hierarchy[cmditem['short_name']].pack(side=Tkinter.TOP, anchor=Tkinter.W)
+                # to pass options not in the command specification (cmditem)
+                # keyword=value_to_pass,
+        notebook.setnaturalsize()
+        # now collapse the entry methods after the maximum window size is known
+        for category_item in categories:
+            cmdtag = control_parse.find_commands(db, 'system', sysname,
+                                                 'category', category_item)
+            for cmditem in cmdtag:
+                if cmditem['edit_level'] == 1:
+                    self.command_hierarchy[cmditem['short_name']].flattenentry()
+        # put in a refresh order
+        self.commandqueue.put('refresh')
+
+    def refresh_system(self, msg):
+        # take everything following, and including the GUI_MESSAGE tag and
+        # parse it into button messages
+        refresh_string = msg[msg.find(GUI_MESSAGE):].split()
+        refresh_shortname = refresh_string[1]
+        refresh_type = refresh_string[2]
+        refresh_val = refresh_string[3]
+        buttons_available = self.command_hierarchy.keys()
+        if buttons_available.count(refresh_shortname) > 1:
+            print 'SystemControlWindow: refresh more than one button error'
+        else:
+            try:
+                buttons_available.index(refresh_shortname)
+                print 'hwid: ' + repr(refresh_shortname) + \
+                      ' type: ' + repr(refresh_type) + \
+                      ' val: ' + repr(refresh_val)
+
+                self.command_hierarchy[refresh_shortname].refresh_button(refresh_type, refresh_val)
+            except ValueError:
+                pass
+                print 'This system has no button named %s' % refresh_shortname
