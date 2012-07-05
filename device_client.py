d104c961b4a40534795fb3cf1c379261afbf5bbf
Eric Switzer <eric.switzer@gmail.com>
2012-07-05 14:45:35 -0400

    preliminary device client working

--- device_client.py
+++ device_client.py
@@ -2,16 +2,13 @@ import os
 import redis
 import control_parse
 import threading
-import queue
-# connect to redis with subthread
-# parse json file
-# event handler for incoming: queue
-# issue: sequenctial execution; may miss incoming command if sent during
-# execution
+from Queue import Queue
+import time
+# TODO: have ctrl-C kill this more gracefully
 
 class RedisSubscribeQueue(threading.Thread):
     """Connect to redis and subscribe to a channel, wx-publishing msg"""
-    def __init__(self, pool, subname="commanding"):
+    def __init__(self, pool, command_queue, subname="commanding"):
         """
         `pool`: the redis connection pool for this instance
         `subname`: subscriber channel on the redis server for the thread
@@ -20,6 +17,7 @@ class RedisSubscribeQueue(threading.Thread):
         self.pool = pool
         self.subname = subname
         self.redis_conn = redis.Redis(connection_pool=self.pool)
+        self.command_queue = command_queue
 
         self.redis_pubsub = self.redis_conn.pubsub()
         # subscribe to both the general and specific client channels
@@ -31,20 +29,39 @@ class RedisSubscribeQueue(threading.Thread):
             self.handle_msg(msg)
 
     def handle_msg(self, msg):
-        r"""Do simple parsinig of the message from the server"""
-        print msg
+        r"""Do simple parsing of the message from the server"""
+        self.command_queue.put(msg)
+        print msg['data']
 
 
 class DeviceClient(object):
-    def __init__(self, parent=None, id=-1):
+    def __init__(self, subname="commanding", ackname="commanding_ack"):
         configaddr = "http://www.cita.utoronto.ca/~eswitzer/master.json"
+        self.command_queue = Queue()
         self.config = control_parse.ControlSpec(configaddr=configaddr,
                                                 silent=False)
 
         self.pool = redis.ConnectionPool(host="localhost", port=6379, db=0)
         self.redis_conn = redis.Redis(connection_pool=self.pool)
 
-        self.acksub = network_client.RedisSubscribeQueue(self.pool,
-                                                         self.client_id,
-                                                subname="commanding_ack")
+        self.commandsub = RedisSubscribeQueue(self.pool, self.command_queue,
+                                              subname=subname)
 
+        while True:
+            item = self.command_queue.get()
+            print "starting on: ", item
+            # working really hard now:
+            time.sleep(5)
+            print "done"
+            command = item['data'].split()
+            if len(command) == 2:
+                self.redis_conn.set(command[0], command[1])
+                self.redis_conn.publish(ackname, " ".join(command))
+            else:
+                print "malformed command: ", " ".join(command)
+
+            self.command_queue.task_done()
+
+
+if __name__ == "__main__":
+    devclient = DeviceClient()
